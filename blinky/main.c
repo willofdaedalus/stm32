0a81c61d9f0305eaec0d8b8253c86ed895a11843
willofdaedalus <manny.is.daedalus@pm.me>
2025-10-13 06:40:24 +0000

    bitwise operators on blinky

--- blinky/main.c
+++ blinky/main.c
@@ -8,12 +8,11 @@ int main(void)
 	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN | RCC_AHB1ENR_GPIOCEN;
 
 	// clear the bits pre-existing
-	GPIOA->MODER &= ~GPIO_MODER_MODER5;
+	GPIOA->MODER &= ~(3 << (5 * 2));
 	// enable general output mode
-	GPIOA->MODER |= GPIO_MODER_MODER5_0;
+	GPIOA->MODER |= (1 << (5 * 2));
 
 	while (1) {
-		// works but needs button debouncing
 		if (!(GPIOC->IDR & GPIO_IDR_ID13)) {
 			// crude debounce delay
 			for (i = 0; i < 100000; i++)
