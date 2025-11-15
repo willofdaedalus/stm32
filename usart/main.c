ab039a11cdf080fb7b8092825c036f91497a4cc1
willofdaedalus <manny.is.daedalus@pm.me>
2025-11-15 14:15:48 +0000

    logical changes

--- usart/main.c
+++ usart/main.c
@@ -1,36 +1,39 @@
 #include "stm32f4xx.h"
 
-#define TX_PORT (2)
-#define RX_PORT (3)
+#define TX_PIN (2)
+#define RX_PIN (3)
+#define APB1_CLOCK (16000000)
+#define BAUDRATE (9600)
 
 void gpio_init(void)
 {
 	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
-	// configure pa2 and pa3 as alternate function ports
-	GPIOA->MODER &= ~(3 << (TX_PORT * 2)) | ~(3 << (RX_PORT * 2));
-	GPIOA->MODER |= (2 << (TX_PORT * 2)) | (2 << (RX_PORT * 2));
 
-	// enable the alternate function number (AF7)
-	GPIOA->AFR[0] &= ~((0xf << (4 * 2)) | (0xf << (4 * 3)));
-	GPIOA->AFR[0] |= (7 << (TX_PORT * 4)) | (7 << (RX_PORT * 4));
+	// clear mode bits
+	GPIOA->MODER &= ~((3 << (TX_PIN * 2)) | (3 << (RX_PIN * 2)));
+	GPIOA->MODER |= (2 << (TX_PIN * 2)) | (2 << (RX_PIN * 2));
+
+	// configure af7 for usart2
+	GPIOA->AFR[0] &= ~((0xF << (TX_PIN * 4)) | (0xF << (RX_PIN * 4)));
+	GPIOA->AFR[0] |= (7 << (TX_PIN * 4)) | (7 << (RX_PIN * 4));
 }
 
 void usart_init(void)
 {
-	// enable usart2 clock interface
 	RCC->APB1ENR |= RCC_APB1ENR_USART2EN;
-	USART2->BRR = 0x0683; // 9600 baud @ 16 MHz
-	// enable usart UE
-	USART2->CR1 |= USART_CR1_UE | USART_CR1_TE | USART_CR1_RE;
+
+	USART2->CR1 = USART_CR1_TE | USART_CR1_RE;
+	USART2->BRR = APB1_CLOCK / BAUDRATE;
+	USART2->CR1 |= USART_CR1_UE;
 }
 
 void usart_display(const char *str)
 {
 	while (*str) {
-		while (!(USART2->SR & USART_SR_TXE))
-			;
+		while (!(USART2->SR & USART_SR_TXE)) {
+		}
 
-		USART2->DR = *str++;
+		USART2->DR = (uint8_t) *str++;
 	}
 }
 
@@ -44,6 +47,6 @@ int main(void)
 	while (1) {
 		usart_display(str);
 		for (volatile int i = 0; i < 1000000; i++)
-			; // small delay
+			;
 	}
 }
